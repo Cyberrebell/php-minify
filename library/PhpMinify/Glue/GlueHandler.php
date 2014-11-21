7fb59882cc82b881dc61af7fa862fc1a6b5cd13e
stephan <stephan@debian>
2014-11-21 21:28:43 +0100

    file format check

--- library/PhpMinify/Glue/GlueHandler.php
+++ library/PhpMinify/Glue/GlueHandler.php
@@ -4,5 +4,29 @@ namespace PhpMinify\Glue;
 
 class GlueHandler
 {
+	protected $inputFiles;
+	protected $outputFile;
+	protected $fileFormat;
 	
+	function __construct(array $inputFiles, $outputFile) {
+		$this->inputFiles = $inputFiles;
+		$this->outputFile = $outputFile;
+		$this->checkFileFormatConsistence();
+	}
+	
+	protected function checkFileFormatConsistence() {
+		foreach ($this->inputFiles as $inputFile) {
+			$ending = substr($inputFile, strpos($inputFile, '.', -1));
+			if ($this->fileFormat) {
+				if ($ending != $this->fileFormat) {
+					throw new \Exception("invalid glue configuration. Can't glue ." . $ending . " file with ." . $this->fileFormat);
+				}
+			} else {
+				$this->fileFormat = $ending;
+			}
+		}
+		if ($this->fileFormat != $this->outputFile) {
+			throw new \Exception("invalid glue configuration. Can't convert ." . $this->fileFormat . " files to ." . $this->outputFile);
+		}
+	}
 }
